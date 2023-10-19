5f4350e68ea5fa24477dd5315aeeedbf034cdd17
Rishabh Gupta <rishabh9aug@gmail.com>
2023-10-19 18:41:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/livedatarevision/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.livedatarevision
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
